c43ff8c11dc6621d1d0cb28b0251a081d6716828
Ruben Moses <103294013+MosesCodeER@users.noreply.github.com>
2022-06-09 23:27:54 -0700

    Add files via upload

---
+++ student.js
@@ -0,0 +1,7 @@
+
+ let myname = "Ruben Moses";
+ let my_message = "cs190 Javascript Lab 2";
+ let id2 = 506;
+ function displayinfo602 ( ) { 
+    alert ( "My name is " + myname + " My Message is " + my_message + "My id is " + id2);
+}  
